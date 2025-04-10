b1c39366f7507e00eb9ac1db214fdc4c03c39b6f
Soham <darakss@rknec.edu>
2025-04-10 16:22:05 +0530

    Changes in Vite file

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  bash: process.env.VITE_BASE_PATH || "/CEP_AgriMed",
   server: {
     proxy: {
       '/api': {
